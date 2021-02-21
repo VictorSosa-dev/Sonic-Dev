1ba753e73602b027b1f35fd40850c353c0bab84b
Cristofer Alvarado <luiscristofer.alvarado@gmail.com>
2021-02-21 12:46:52 -0600

    Cambios finales para fusionar

--- src/main/java/mx/uam/ayd/proyecto/presentacion/reporteVenta/ControlMuestraReportesVenta.java
+++ src/main/java/mx/uam/ayd/proyecto/presentacion/reporteVenta/ControlMuestraReportesVenta.java
@@ -8,7 +8,11 @@ import org.springframework.stereotype.Component;
 import mx.uam.ayd.proyecto.negocio.ServicioReporteVenta;
 import mx.uam.ayd.proyecto.negocio.modelo.Empleado;
 import mx.uam.ayd.proyecto.negocio.modelo.ReporteVenta;
-
+/**
+ * 
+ * @author Luis Cristofer Alvarado Gabriel
+ * @since 21/02/2021
+ */
 @Component
 public class ControlMuestraReportesVenta {
 
@@ -18,6 +22,10 @@ public class ControlMuestraReportesVenta {
 	@Autowired 
 	ServicioReporteVenta servicioReporteVenta;
 	
+	/**
+	 * Metodo que inicia la ventana de la lista de ventas reportadas
+	 * @param empleado
+	 */
 	public void inicia(Empleado empleado) {
 		List<ReporteVenta> reportesVentas = servicioReporteVenta.recuperaReportes();
 		ventana.muestra(this, empleado, reportesVentas);
